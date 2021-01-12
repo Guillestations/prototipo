47d322a3d401497dbb6a1615043e41ab6063a2e9
Guille <guillejuanmanuel@gmail.com>
2021-01-12 20:19:22 -0300

    Prototipo de QR terminado

---
+++ app.js
@@ -0,0 +1,19 @@
+let qrCodigo = select("img");
+let qrTexto = select("textarea");
+let qrBoton = select("button");
+
+qrBoton.addEventListener("click", generadorQR);
+
+function generadorQR(){
+    let size = "1000x1000";
+    let datos = qrTexto.value;
+    let baseURL = "https://api.qrserver.com/v1/create-qr-code/";
+
+    let url = `${baseURL}?data=${datos}&size=${size}`;
+
+    qrCodigo.src = url;
+}
+
+function select (elemento){
+    return document.querySelector(elemento);
+}
