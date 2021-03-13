8ff128b7a1f2c50d5bf3e48bbba1c34ac0bad7a6
Guille <guillejuanmanuel@gmail.com>
2021-03-13 07:30:41 -0300

    Se agregaron detalles

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 let qrCodigo = select("img");
-let qrTexto = select("textarea");
+let qrTexto = select("input");
+let texto = document.getElementById('texto').value;
 let qrBoton = select("button");
 
+console.log(texto)
 qrBoton.addEventListener("click", generadorQR);
 
 function generadorQR(){
-    let size = "1000x1000";
-    let datos = qrTexto.value;
-    let baseURL = "https://api.qrserver.com/v1/create-qr-code/";
 
-    let url = `${baseURL}?data=${datos}&size=${size}`;
+    if(texto != ''){
+        let size = "1000x1000";
+        let datos = qrTexto.value;
+        let baseURL = "https://api.qrserver.com/v1/create-qr-code/";
 
-    qrCodigo.src = url;
+        let url = `${baseURL}?data=${datos}&size=${size}`;
+
+        qrCodigo.src = url;
+        document.querySelector('#texto').value= '';
+    }else{
+        confirm('debe ingresar texto/link para convertir a QR')
+    
+    }
+    
 }
 
 function select (elemento){
     return document.querySelector(elemento);
+}
+
+const validar = (e) => {
+    
 }
 
